19ece4df16b199302fbde910b186947c3d8d9928
KamiliArsyad <arsyad.ik@gmail.com>
2022-05-25 22:02:57 +0800

    added user controller skeleton

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = (req, res) => {};
+
+// @desc    Log user in
+// @route   POST /api/users
+// @access  Public
+const loginUser = (req, res) => {};
+
+// @desc    Get user data
+// @route   Get /api/users/me
+// @access  Private
+const getUser = (req, res) => {};
+
+module.exports = { registerUser, loginUser, getUser };
--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use("/api/users", require('./routes/userRoutes'));
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
