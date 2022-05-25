91fc7c0d889b56bced7ac441d6186218f59415eb
KamiliArsyad <arsyad.ik@gmail.com>
2022-05-25 17:54:18 +0800

    setup server and env for express

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv").config;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
