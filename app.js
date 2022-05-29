e5bc3546d1b1e6ba562e502f7282c5b5938cae8a
Florentiana <e0851439@u.nus.edu>
2022-05-30 02:04:52 +0800

    Change dashboard styling, add basic recommendation feature

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://DigitalIT:startdigitalizing@digitalit.mj9p3.mongodb.net/?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// }, function(error) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log("Connected to the database");
+//     }
+// });
+
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set the public folder as external file folder
 app.use(express.static("public"));
@@ -20,23 +34,38 @@ app.get("/signup", function(request, response) {
     response.render("signup");
 })
 
-// Mock api request
-app.get("/dashboard", function(request, response) {
-    var name = request.query.name;
-    var email = request.query.email;
-    var password = request.query.password;
-    var phone = request.query.phone;
+// //POST Method for /signup route
+// app.post("/signup", function(req, res){
+//     var data = req.body;
+//     userData.push(data);
+//     res.redirect("/dashboard");
+// });
 
-    request("api-page-link&email=" + email + "&password=" + password, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            var data = JSON.parse(body);
-            res.render("dashboard", {
-                userData: data
-            });
-        }
-    });
+// // Mock api request
+// app.get("/dashboard", function(request, response) {
+//     var name = request.query.name;
+//     var email = request.query.email;
+//     var password = request.query.password;
+//     var phone = request.query.phone;
+
+//     request("api-page-link&email=" + email + "&password=" + password, function (error, response, body) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             var data = JSON.parse(body);
+//             res.render("dashboard", {
+//                 userData: data
+//             });
+//         }
+//     });
+// })
+
+app.get("/dashboard", function(request, response) {
+    response.render("dashboard");
+})
+
+app.get("/tokopedia", function(request, response) {
+    response.render("tokopedia");
 })
 
 app.get("*", function(request, response) {
