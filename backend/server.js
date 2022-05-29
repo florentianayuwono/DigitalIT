6815c9fef1910c1221469692e4a7a9a75436d1f2
KamiliArsyad <arsyad.ik@gmail.com>
2022-05-29 12:53:23 +0800

    Silly bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const cors = require("cors");
-const dotenv = require("dotenv").config;
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const express = require("express");
 const PORT = process.env.PORT || 5000;
