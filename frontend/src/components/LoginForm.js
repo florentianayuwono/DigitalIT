07fdfdc242f9fd3aa33e54d9b4732df5bafbc985
KamiliArsyad <arsyad.ik@gmail.com>
2022-06-17 12:15:29 +0800

    Modify link for deployment

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -10,7 +10,7 @@ const LoginForm = () => {
 
     try {
       const body = { "email": email, "password": password };
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
