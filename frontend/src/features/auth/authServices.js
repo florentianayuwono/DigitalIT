9d2aa6b961e86c72d0d84fa5f4074c80a7d68d7c
KamiliArsyad <arsyad.ik@gmail.com>
2022-06-23 07:01:56 +0800

    fixed typo

--- frontend/src/features/auth/authServices.js
+++ frontend/src/features/auth/authServices.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 
 // !! Add http://localhost:5000 before /api for local testing.
 const API_LINK = process.env.NODE_ENV === "production" 
-  ? "/api/users"
+  ? "/api/users/"
   : "http://localhost:5000/api/users/";
 
 export const loginUser = async (dispatch, loginPayload) => {
