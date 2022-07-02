252fd2f259262738a786c01b86a5474eda4c3d4a
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-02 15:51:43 +0800

    (not tested) completed basic product feature

--- frontend/src/features/product/productReducer.js
+++ frontend/src/features/product/productReducer.js
@@ -3,9 +3,32 @@ Structure of products state:
 {id: product}
 where product is of structure: {
   id: number,
-  
+
 }
 */
+
+function productCreator(rawProduct) {
+  const { id, product_name, product_description, price, cost } = rawProduct;
+  return {
+    id,
+    product: {
+      product_name,
+      product_description,
+      price,
+      cost,
+    },
+  };
+}
+
+function productWithoutID(rawProduct) {
+  const { product_name, product_description, price, cost } = rawProduct;
+  return {
+    product_name,
+    product_description,
+    price,
+    cost,
+  };
+}
 
 export const initialProductState = {
   products: {},
@@ -13,4 +36,52 @@ export const initialProductState = {
   isError: false,
   isSuccess: false,
   message: "",
+};
+
+export const productReducer = (state, action) => {
+  switch (action.type) {
+    case "RESET":
+      return initialProductState;
+    case "REQUEST_PRODUCT":
+    case "REQUEST_PRODUCTS":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+        isSuccess: false,
+        message: "",
+      };
+    case "GET_PRODUCTS_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        isSuccess: true,
+        message: "",
+        products: action.payload.map(productCreator),
+      };
+    case "GET_PRODUCT_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        isSuccess: true,
+        message: "",
+        products: {
+          ...state.products,
+          [action.payload.id]: productWithoutID(action.payload),
+        },
+      };
+    case "GET_PRODUCTS_ERROR":
+    case "GET_PRODUCT_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        isSuccess: false,
+        message: action.error,
+      };
+    default:
+      return state;
+  }
 };
