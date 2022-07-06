8acc19ec7217807265219c816ed5fb0437696b47
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-06 23:46:49 +0800

    Added comment

--- frontend/src/pages/BusinessParticular/DisplayIndividualBusiness.jsx
+++ frontend/src/pages/BusinessParticular/DisplayIndividualBusiness.jsx
@@ -1,6 +1,25 @@
 import { Outlet, useParams } from "react-router-dom";
 import { useBusinessContext } from "../../features/business/businessContext";
 
+
+/*
+  Improvement for later here!!
+
+  The way this DisplayIndividualBusiness page works after the modification in which
+  we navigate to this page after selecting a business, passing only the business_id
+  to this component and not the whole business item which we already have in the previous
+  page, is that we have to search for this business item later in the global business state.
+
+  As of right now, the businesses are stored in an array of businesses (specified in the
+  businessReducer file) and we need to do linear search for the business item that we
+  want to display. This is a very inefficient way of doing this.
+
+  An improvement would be to store the businesses in an object that has the business_id as
+  the key and the business item as the value. This would be much more efficient as we can
+  index in O(1) time. To achieve this, we can imitate how the products are being stored in 
+  the productReducer file. Once that's done, remember to modify the function below so that
+  it's suitable for the modified data structure.
+*/
 const selectBusiness = (business_id, businesses) => {
   const business = businesses.businesses.find(
     (business) => parseInt(business.business_id) === parseInt(business_id)
