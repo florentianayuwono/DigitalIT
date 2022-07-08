3a1d231488a60537b6dd6e0135c85bcdbce96406
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-08 16:44:39 +0800

    Improved delete button (now with noice alert :))

--- frontend/src/pages/Product/Products.jsx
+++ frontend/src/pages/Product/Products.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { useParams, useNavigate, Outlet } from "react-router-dom";
+import PopupMessageButton from "../../components/PopupMessageButton";
 import { useProductContext } from "../../features/product/productContext";
-import { deleteProduct, getProducts } from "../../features/product/productServices";
+import {
+  deleteProduct,
+  getProducts,
+} from "../../features/product/productServices";
 
 export default function Products() {
   const business_id = useParams().business_id;
@@ -30,27 +34,32 @@ export default function Products() {
       console.error(err.message);
       return;
     }
-  }
+  };
 
   return (
     <div className="row align-items-md-stretch">
-      {Object.keys(products.products).filter(key => products.products[key] !== undefined).map((key) => {
-        const product = products.products[key];
+      {Object.keys(products.products)
+        .filter((key) => products.products[key] !== undefined)
+        .map((key) => {
+          const product = products.products[key];
 
-        return (
-          <div className="col-md-6" key={key}>
-            <div className="h-100 p-5 bg-light border rounded-3" key={key}>
-              <h3>{product.product_name}</h3>
-              <p> Description: {product.product_description}</p>
-              <p> Price: {product.price}</p>
-              <p> Cost: {product.cost}</p>
-              <button className="btn btn-primary" onClick={() => deleteAction(key)}>
-                Delete
-              </button>
+          return (
+            <div className="col-md-6" key={key}>
+              <div className="h-100 p-5 bg-light border rounded-3" key={key}>
+                <h3>{product.product_name}</h3>
+                <p> Description: {product.product_description}</p>
+                <p> Price: {product.price}</p>
+                <p> Cost: {product.cost}</p>
+                <PopupMessageButton
+                  action={() => deleteAction(key)}
+                  message="Are you sure to delete this product?"
+                  title="Delete"
+                  executeTitle="Delete"
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       <div className="col-md-6">
         <div
           className="btn h-100 p-5 border-solid rounded-3"
