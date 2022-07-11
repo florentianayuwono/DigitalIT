d85123af7ff55636b4a7884062e65cbd228c68ba
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-11 23:54:57 +0800

    Not tested (new db config)

--- backend/config/database.sql
+++ backend/config/database.sql
@@ -20,8 +20,9 @@ CREATE TABLE business (
   business_id SERIAL UNIQUE NOT NULL,
   manager_id uuid NOT NULL,
   business_name VARCHAR(255) NOT NULL,
-  categories VARCHAR(255) NOT NULL,
+  business_category VARCHAR(255) NOT NULL,
   has_digitalized BOOLEAN,
+  creation_date DATE NOT NULL,
   PRIMARY KEY (business_id),
   FOREIGN KEY (manager_id) REFERENCES user_account (user_id)
 );
@@ -30,21 +31,63 @@ CREATE TABLE business (
 INSERT INTO business (manager_id, business_name, categories, has_digitalized)
 VALUES ('60dc16dd-c7f1-4fde-827a-90c0e101555c', 'my business', 'electronics', 'yes');
 
-CREATE TABLE product (
-  product_id SERIAL UNIQUE NOT NULL,
-  business_id INT NOT NULL,
+CREATE TABLE product_main (
+  product_id uuid DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
   product_name VARCHAR(255) NOT NULL,
-  product_description TEXT,
-  price NUMERIC,
-  cost NUMERIC,
-  PRIMARY KEY (product_id),
-  FOREIGN KEY (business_id) REFERENCES business (business_id)
+  product_description VARCHAR(255) NOT NULL,
+  product_category VARCHAR(255) NOT NULL,
+  product_importance INT NOT NULL,
+  age_target INT NOT NULL,
+  gender_target INT NOT NULL,
+  PRIMARY KEY (product_id)
+);
+
+CREATE TABLE product_secondary (
+  product_local_id uuid DEFAULT uuid_generate_v4 UNIQUE NOT NULL,
+  store_id SERIAL NOT NULL,
+  business_id SERIAL NOT NULL,
+  product_id uuid NOT NULL,
+  product_cost NUMERIC NOT NULL,
+  product_price NUMERIC NOT NULL,
+  PRIMARY KEY (product_local_id),
+  FOREIGN KEY (product_id) REFERENCES product_main (product_id),
+  FOREIGN KEY (business_id) REFERENCES business (business_id),
+  FOREIGN KEY (store_id) REFERENCES store (store_id)
+);
+
+CREATE TABLE product_sales (
+  transaction_id SERIAL UNIQUE NOT NULL,
+  product_id uuid NOT NULL,
+  product_local_id uuid NOT NULL,
+  store_id INT NOT NULL,
+  input_date DATE NOT NULL,
+  date_range INT NOT NULL,
+  quantity INT NOT NULL,
+  product_cost NUMERIC NOT NULL,
+  product_price NUMERIC NOT NULL,
+  individual_profit INT NOT NULL,
+  total_profit INT NOT NULL,
+  PRIMARY KEY (transaction_id),
+  FOREIGN KEY (product_id) REFERENCES product (product_id),
+  FOREIGN KEY (product_local_id) REFERENCES product_secondary (product_local_id),
+  FOREIGN KEY (store_id) REFERENCES store (store_id)
+);
+
+CREATE TABLE platform (
+  platform_id SERIAL UNIQUE NOT NULL,
+  platform_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (platform_id)
 );
 
 CREATE TABLE store (
   store_id SERIAL UNIQUE NOT NULL,
-  business_id INT NOT NULL,
-  platform VARCHAR(255),
+  store_name VARCHAR(255) NOT NULL,
+  business_id SERIAL NOT NULL,
+  store_manager_id uuid NOT NULL,
+  store_platform_id SERIAL NOT NULL,
+  creation_date DATE NOT NULL,
   PRIMARY KEY (store_id),
-  FOREIGN KEY (business_id) REFERENCES business (business_id)
+  FOREIGN KEY (business_id) REFERENCES business (business_id),
+  FOREIGN KEY (store_manager_id) REFERENCES business (manager_id),
+  FOREIGN KEY (store_platform_id) REFERENCES platform (platform_id)
 );
