163b8972ba75f210c8b2f913bd1ac0656ecf696a
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-12 07:32:37 +0800

    Created new database (command ran on heroku)

--- backend/config/database.sql
+++ backend/config/database.sql
@@ -1,3 +1,7 @@
+-- COMMAND:
+-- cmd /c 'heroku pg:psql postgresql-symmetrical-74289 --app orbital-digital-it < ./backend/config/database.sql'
+-- experimental: postgresql-perpendicular-35193
+
 -- CREATE DATABASE digitalit;
 CREATE EXTENSION CITEXT;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -41,9 +45,27 @@ CREATE TABLE product_main (
   gender_target INT NOT NULL,
   PRIMARY KEY (product_id)
 );
+CREATE TABLE platform (
+  platform_id SERIAL UNIQUE NOT NULL,
+  platform_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (platform_id)
+);
+
+CREATE TABLE store (
+  store_id SERIAL UNIQUE NOT NULL,
+  store_name VARCHAR(255) NOT NULL,
+  business_id SERIAL NOT NULL,
+  store_manager_id uuid NOT NULL,
+  store_platform_id SERIAL NOT NULL,
+  creation_date DATE NOT NULL,
+  PRIMARY KEY (store_id),
+  FOREIGN KEY (business_id) REFERENCES business (business_id),
+  FOREIGN KEY (store_manager_id) REFERENCES user_account (user_id),
+  FOREIGN KEY (store_platform_id) REFERENCES platform (platform_id)
+);
 
 CREATE TABLE product_secondary (
-  product_local_id uuid DEFAULT uuid_generate_v4 UNIQUE NOT NULL,
+  product_local_id uuid DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
   store_id SERIAL NOT NULL,
   business_id SERIAL NOT NULL,
   product_id uuid NOT NULL,
@@ -68,26 +90,7 @@ CREATE TABLE product_sales (
   individual_profit INT NOT NULL,
   total_profit INT NOT NULL,
   PRIMARY KEY (transaction_id),
-  FOREIGN KEY (product_id) REFERENCES product (product_id),
+  FOREIGN KEY (product_id) REFERENCES product_main (product_id),
   FOREIGN KEY (product_local_id) REFERENCES product_secondary (product_local_id),
   FOREIGN KEY (store_id) REFERENCES store (store_id)
-);
-
-CREATE TABLE platform (
-  platform_id SERIAL UNIQUE NOT NULL,
-  platform_name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (platform_id)
-);
-
-CREATE TABLE store (
-  store_id SERIAL UNIQUE NOT NULL,
-  store_name VARCHAR(255) NOT NULL,
-  business_id SERIAL NOT NULL,
-  store_manager_id uuid NOT NULL,
-  store_platform_id SERIAL NOT NULL,
-  creation_date DATE NOT NULL,
-  PRIMARY KEY (store_id),
-  FOREIGN KEY (business_id) REFERENCES business (business_id),
-  FOREIGN KEY (store_manager_id) REFERENCES business (manager_id),
-  FOREIGN KEY (store_platform_id) REFERENCES platform (platform_id)
 );
