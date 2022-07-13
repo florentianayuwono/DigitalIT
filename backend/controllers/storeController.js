62e8a41fbd1636323c7b9f9ab4ed04e0576b1d5a
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-13 16:20:21 +0800

    Working store controller API feature

--- backend/controllers/storeController.js
+++ backend/controllers/storeController.js
@@ -1,39 +1,51 @@
 const asyncHandler = require("express-async-handler");
 const pool = require("../config/db");
+const getDate = require("../auxiliaries/helperFunctions").getDate;
 
 // @desc    Add new store
 // @route   POST /api/store/
 // @access  Private
 const addStoreData = asyncHandler(async (req, res) => {
-  const { storeName, business_id } = req.body;
-  const manager_id = await pool.query(
-    "SELECT manager_id from business WHERE business_id = $1",
+  const { business_id, platform_id } = req.body;
+  const business = await pool.query(
+    "SELECT * from business WHERE business_id = $1",
     [business_id]
   );
+
   // Check whether the user who tries to add new store is indeed the business owner
-  if (manager_id.rows[0].manager_id !== req.user.user_id) {
+  if (business.rows[0].manager_id !== req.user.user_id) {
     res.status(401);
-    throw new Error("You do not own this store.");
+    throw new Error("You do not own this business.");
   }
 
   const newStore = await pool.query(
-    "INSERT INTO store (business_id, platform) VALUES ($1, $2) RETURNING *",
-    [business_id, storeName]
+    "INSERT INTO store (store_name, business_id, store_manager_id, store_platform_id, creation_date) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [
+      business.rows[0].business_name,
+      business_id,
+      business.rows[0].manager_id,
+      platform_id,
+      getDate(),
+    ]
   );
 
-  res.status(201).json(newStore.rows[0]);
+  res
+    .status(newStore.rows[0] ? 201 : 400)
+    .json(
+      newStore.rows[0] ? newStore.rows[0] : { message: "Failed to add store." }
+    );
 });
 
 // @desc    Get store data
-// @route   GET /api/store/:id
+// @route   GET /api/store/:store_id
 // @access  Private
 const getStoreData = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { store_id } = req.params;
 
-  const { business_id } = req.body;
+  const { business_id } = req.headers;
 
   const manager_id = await pool.query(
-    "SELECT manager_id from business WHERE business_id = $1",
+    "SELECT * from business WHERE business_id = $1",
     [business_id]
   );
   // Check whether the user who tries to access store data is indeed the business owner
@@ -41,21 +53,22 @@ const getStoreData = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error("You do not own this store.");
   }
+
   // If there is id specified, return the specified store
-  if (id) {
+  if (store_id) {
     const specificStore = await pool.query(
-      "SELECT s.platform FROM store AS s LEFT JOIN business AS b ON b.business_id = s.business_id WHERE b.business_id = $1 AND s.store_id = $2",
-      [business_id, id]
+      "SELECT * FROM store AS s LEFT JOIN business AS b ON b.business_id = s.business_id WHERE b.business_id = $1 AND s.store_id = $2",
+      [business_id, store_id]
     );
-    res.status(200).json(specificStore.rows[0]);
+    res.status(specificStore.rows[0] ? 200 : 400).json(specificStore.rows[0] || {message: "Store not found."});
     // Since there is no id specified, return all the stores that this business has
   } else {
     const store = await pool.query(
-      "SELECT s.platform FROM store AS s LEFT JOIN business AS b ON b.business_id = s.business_id WHERE b.business_id = $1",
+      "SELECT * FROM store AS s LEFT JOIN business AS b ON b.business_id = s.business_id WHERE b.business_id = $1",
       [business_id]
     );
 
-    res.status(200).json(store.rows);
+    res.status(store.rows ? 200 : 400).json(store.rows || {message: "No stores found."});
   }
 });
 
