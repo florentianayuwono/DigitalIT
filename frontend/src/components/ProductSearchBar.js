a43c60be873c65bb0d1408d0afff567c825054c3
KamiliArsyad <arsyad.ik@gmail.com>
2022-07-18 00:40:16 +0800

    Restructure product search

--- frontend/src/components/ProductSearchBar.js
+++ frontend/src/components/ProductSearchBar.js
@@ -5,33 +5,19 @@ import {
   FormErrorMessage,
   FormHelperText,
   Button,
-  Container,
-  RadioGroup,
-  Stack,
-  Radio,
-  Box,
-  InputGroup,
-  InputRightAddon,
-  InputLeftAddon,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { searchProduct } from "../features/product/productServices";
+import ProductSearchResultItem from "./ProductSearchResultItem";
 
 export default function ProductSearchBar() {
   const [keyword, setKeyword] = useState("");
   const [search, setSearch] = useState([]);
-  const [selected, setSelected] = useState({});
-  console.log(selected);
 
   const handleSearch = async (e) => {
     e.preventDefault();
     const result = await searchProduct(keyword);
     setSearch(result);
-  };
-
-  const handleAddProduct = async (e) => {
-    // e.preventDefault;
-    // const product = selected
   };
 
   return (
@@ -52,13 +38,7 @@ export default function ProductSearchBar() {
       <div>
         {/* FOR IMPROVEMENTS: Show only products that are not on the store yet */}
         {search.map((product) => (
-          <InputGroup>
-            {/* FOR IMPROVEMENTS: Add modal here for left addon to show full product description */}
-            <InputLeftAddon minWidth={"400"} children={product.product_name} />
-            <Input type="number" maxWidth={"120"} placeholder="cost" />
-            <Input type="number" maxWidth={"120"} placeholder="price" />
-            <InputRightAddon children={<Button>Add Product to Store</Button>} />
-          </InputGroup>
+          <ProductSearchResultItem product={product} />
         ))}
       </div>
     </>
