8e3f8196eb433a81cea5acc60aaa176b6a34dc6b
Arsyad Kamili <arsyad.ik@gmail.com>
2022-07-24 11:31:33 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <ChakraProvider>
       <div className="container">
         {/* Make the header bar always appears in every page */}
         <Header />
         {/* Render the page based on the routing specified */}
         <Outlet />
       </div>
-    </ChakraProvider>
   );
 }
 
