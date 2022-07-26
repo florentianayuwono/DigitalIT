15f2970daac36fd37d6d93095da38232440e59c4
Florentiana <e0851439@u.nus.edu>
2022-07-26 12:29:59 +0800

    static finance recommendation

--- frontend/src/pages/Finance/FinanceRecommendations.jsx
+++ frontend/src/pages/Finance/FinanceRecommendations.jsx
@@ -1,7 +1,65 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  FormControl,
+  Select,
+  FormLabel,
+  Button,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function FinanceRecommendations() {
   return (
     <div>
-      <h1>Finance Recommendations</h1>
+      <Accordion allowMultiple allowToggle>
+        <AccordionItem>
+          <h2>
+            <AccordionButton _expanded={{ bg: "purple", color: "white" }}>
+              <container flex="1" textAlign="left">
+                Enemy in disguise
+              </container>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            You might want to watch out for your Cost of Goods Sold or
+            Operational Expense! Looks like it is hindering your profit growth.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton _expanded={{ bg: "purple", color: "white" }}>
+              <container flex="1" textAlign="left">
+                Paradox of thrift in action
+              </container>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            You might be able to boost your revenue by spending more in
+            promotion! Check out our business recommendations for more tips.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton _expanded={{ bg: "purple", color: "white" }}>
+              <container flex="1" textAlign="left">
+                Digitalize your bookkeeping
+              </container>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Tired of filling sales data manually on pen and paper? Why not try
+            these awesome bookkeeping platforms.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
